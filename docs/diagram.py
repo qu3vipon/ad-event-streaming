604671d5f119bc435d0b86634bd5f74b36c9c925
qu3vipon <qu3vipon@gmail.com>
2023-01-21 11:26:21 +0900

    docs: update diagram

--- docs/diagram.py
+++ docs/diagram.py
@@ -2,11 +2,11 @@ from diagrams import Diagram, Edge, Cluster
 from diagrams.onprem.client import Client
 from diagrams.onprem.compute import Server
 from diagrams.onprem.database import MongoDB
+from diagrams.onprem.inmemory import Redis
 from diagrams.onprem.queue import Kafka
 
-
 with Diagram("", show=False):
-    with Cluster("Ads System"):
+    with Cluster("Ads System[Async]"):
         with Cluster("Storage"):
             mongo_db = MongoDB("MongoDB")
 
@@ -22,8 +22,13 @@ with Diagram("", show=False):
             client = Client("Client")
             faust_worker = Server("Faust Worker")
             kafka = Kafka("Stream")
+            redis = Redis("Rolling Bloom Filter(e: 0.01, window: last 24h)")
 
             client >> Edge(label="POST /events") >> kafka
 
-            kafka << Edge(label="consume") << faust_worker
-            faust_worker >> Edge(label="charge credit") >> mongo_db
+            kafka << Edge(label="1. consume") << faust_worker
+
+            redis << Edge(label="2. membership query") << faust_worker
+            mongo_db << Edge(label="2-a. query again when possibly duplicate") << faust_worker
+
+            faust_worker >> Edge(label="3. charge credit") >> mongo_db
